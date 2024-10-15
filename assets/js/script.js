eb08105c253d32beb34d5dadbaaee8ee3c69783d
Oleksii Aleferchuk <alex.aleferchuk@gmail.com>
2024-10-15 12:16:29 +0000

    Fix buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+//Wait for the the DOm to be loaded before runninbg the game
+//Get the button elements and add event listener to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type")=== "submit") {
+                alert("You clicked submit");
+            }
+            else {
+                let gameType = this.getAttribute("data-type");
+
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -27,5 +47,5 @@ function displaySubstractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
