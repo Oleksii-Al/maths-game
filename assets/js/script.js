6f376a07e319a225e67242fb4c492f44a7a74735
Oleksii Aleferchuk <alex.aleferchuk@gmail.com>
2024-10-16 14:38:20 +0000

    Fix display division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,7 +95,7 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     }
     else if (operator === "/") {
-        return [Math.floor(operand1 / operand2), "division"];
+        return [operand1 / operand2, "division"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -141,7 +141,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById("operand1").textContent = operand1 * operand2;
+    document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
 }
