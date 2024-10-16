c534c3ed35320d1721405010c87887d48abaa5a1
Oleksii Aleferchuk <alex.aleferchuk@gmail.com>
2024-10-16 13:36:28 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -23,6 +30,11 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+document.getElementById("answer-box").value = "";
+document.getElementById("answer-box").focus();
+
+
     //creates two random number
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
